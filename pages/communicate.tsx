af4a1fc585b0c3680f178d67e116290b6e0717b2
jungyu <jungyu@naver.com>
2022-05-12 21:48:24 +0900

    communicate.tsx css

--- pages/communicate.tsx
+++ pages/communicate.tsx
@@ -12,6 +12,8 @@ const PostContainer = tw.div`
   items-center
   justify-between
   relative
+  hover:bg-slate-100
+  cursor-pointer
 `;
 
 const Badge = tw.span`
@@ -28,14 +30,45 @@ const Badge = tw.span`
   top-1.5
 `;
 
-const PostInfoContainer = tw.div``;
+const PostInfoContainer = tw.div`
+  mt-3
+  flex
+  flex-col
+  items-start
+`;
 
-const Question = tw.div``;
+const Question = tw.div`
+  font-bold
+  text-lg
+  mb-2
+`;
 
-const Name = tw.span``;
+const Name = tw.span`
+  
+  font-medium
+  text-sm
+`;
 
-const PostSubInfoContainer = tw.div``;
+const PostSubInfoContainer = tw.div`
+  flex
+  flex-col
+  space-y-8
+`;
 
+const Time = tw.span`
+  text-gray-400
+  text-md
+`;
+
+const CommentContainer = tw.div`
+  flex
+  items-center
+  justify-center
+`;
+
+const Comment = tw.span`
+  text-sm
+`;
 export default function Communicate() {
   return (
     <Wrapper>
@@ -48,11 +81,24 @@ export default function Communicate() {
               <Name>jungyu</Name>
             </PostInfoContainer>
             <PostSubInfoContainer>
-              <div>11:20</div>
-              <div>
-                <div>2</div>
-                <div>1</div>
-              </div>
+              <Time>11:20</Time>
+              <CommentContainer>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 pr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+                  />
+                </svg>
+                <Comment>2</Comment>
+              </CommentContainer>
             </PostSubInfoContainer>
           </PostContainer>
         </>
