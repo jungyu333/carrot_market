cc07e07aa5e4579f6569242001b45e5877c16ef2
jungyu <jungyu@naver.com>
2022-05-16 15:05:54 +0900

    purchase.tsx & sell.tsx & fav.tsx css

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,3 +1,4 @@
+import type { NextPage } from "next";
 import Link from "next/link";
 import tw from "tailwind-styled-components";
 import Layout from "../../components/layout";
@@ -138,7 +139,7 @@ const Introduce = tw.div`
   p-3
 `;
 
-export default function Profile() {
+const Profile: NextPage = () => {
   return (
     <Layout title="마이페이지" isLogIn hasTabBar canGoBack={false}>
       <Wrapper>
@@ -154,67 +155,75 @@ export default function Profile() {
           </UserInfo>
         </UserWrapper>
         <ListWrapper>
-          <PurchaseContainer>
-            <PurchaseIcon>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-white "
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                />
-              </svg>
-            </PurchaseIcon>
-            <Purchase>구매목록</Purchase>
-          </PurchaseContainer>
-          <SalesContainer>
-            <SalesIcon>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-white "
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </SalesIcon>
-            <Sales>판매목록</Sales>
-          </SalesContainer>
-          <WatchContainer>
-            <WatchIcon>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-white"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-            </WatchIcon>
-            <Watch>관심목록</Watch>
-          </WatchContainer>
+          <Link href="/profile/purchase">
+            <PurchaseContainer>
+              <PurchaseIcon>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-white "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                  />
+                </svg>
+              </PurchaseIcon>
+              <Purchase>구매목록</Purchase>
+            </PurchaseContainer>
+          </Link>
+          <Link href="/profile/sell">
+            <SalesContainer>
+              <SalesIcon>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-white "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </SalesIcon>
+              <Sales>판매목록</Sales>
+            </SalesContainer>
+          </Link>
+          <Link href="/profile/fav">
+            <WatchContainer>
+              <WatchIcon>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </WatchIcon>
+              <Watch>관심목록</Watch>
+            </WatchContainer>
+          </Link>
         </ListWrapper>
         <span className="  text-2xl font-bold">Introduce!</span>
         <Introduce>Hello!</Introduce>
       </Wrapper>
     </Layout>
   );
-}
+};
+
+export default Profile;
